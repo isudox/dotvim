741492352101cb13feaf31850f6856f31dc303d5
isudox <isudox@gmail.com>
2021-05-11 17:51:15 +0800

    Add init shell script.

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/vim-fugitive/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/ale/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/editorconfig-vim/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/jedi-vim/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdcommenter/doc" -c q
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+git submodule update --init --recursive
